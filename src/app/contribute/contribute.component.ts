84742c1bf80c56af33bcbc9574a61aa881058ec6
Vaibhav <vaibhav.jndl@gmail.com>
2018-07-01 16:46:45 +0530

    contribute component styling done

--- src/app/contribute/contribute.component.ts
+++ src/app/contribute/contribute.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-contribute',
